c301d2f9e1b55047a097cf760b25afacd4e465d0
Kaspar Kallas <kaspar@superfluid.finance>
2023-05-04 09:44:22 +0300

    remove stray console.log (#588)

--- src/features/minigame/MinigameContext.tsx
+++ src/features/minigame/MinigameContext.tsx
@@ -1,67 +1,71 @@
-import {PropsWithChildren, useEffect} from "react";
+import { PropsWithChildren, useEffect } from "react";
 import {
-    createContext,
-    FC,
-    useCallback,
-    useContext,
-    useMemo,
-    useState,
+  createContext,
+  FC,
+  useCallback,
+  useContext,
+  useMemo,
 } from "react";
-import {useAccount} from "wagmi";
-import {applySettings} from "../settings/appSettings.slice";
-import {useDispatch} from "react-redux";
-import {useAppLastSuperfluidRunnerCosmetics, useSetting} from "../settings/appSettingsHooks";
+import { useAccount } from "wagmi";
+import { applySettings } from "../settings/appSettings.slice";
+import { useDispatch } from "react-redux";
+import {
+  useAppLastSuperfluidRunnerCosmetics,
+} from "../settings/appSettingsHooks";
 
 const G_A_M_E__U_R_L__B_A_S_E_6_4 =
-    "aHR0cHM6Ly9hc3Ryb2J1bm55LnN1cGVyZmx1aWQuZmluYW5jZS8=";
+  "aHR0cHM6Ly9hc3Ryb2J1bm55LnN1cGVyZmx1aWQuZmluYW5jZS8=";
 
 export type MinigameCosmetics = 1 | 2 | 3 | 4;
 
 type MinigameContextValue = {
-    cosmetics: MinigameCosmetics,
-    setCosmetics: (value: MinigameCosmetics) => void;
-    getUrl: () => URL;
+  cosmetics: MinigameCosmetics;
+  setCosmetics: (value: MinigameCosmetics) => void;
+  getUrl: () => URL;
 };
 
 const MinigameContext = createContext<MinigameContextValue>(null!);
 
-export const MinigameProvider: FC<PropsWithChildren> = ({children}) => {
-    const cosmetics = useAppLastSuperfluidRunnerCosmetics();
-    const dispatch = useDispatch();
+export const MinigameProvider: FC<PropsWithChildren> = ({ children }) => {
+  const cosmetics = useAppLastSuperfluidRunnerCosmetics();
+  const dispatch = useDispatch();
 
-    const {address: connectedAccountAddress} = useAccount(); // Don't use "visible address" here.
+  const { address: connectedAccountAddress } = useAccount(); // Don't use "visible address" here.
 
-    const setCosmetics = useCallback((cosmetics: MinigameCosmetics) => {
-        console.log("I am ere.",cosmetics)
-        dispatch(applySettings({lastSuperfluidRunnerCosmetics: cosmetics}));
-    }, [cosmetics])
+  const setCosmetics = useCallback(
+    (cosmetics: MinigameCosmetics) =>
+      void dispatch(
+        applySettings({ lastSuperfluidRunnerCosmetics: cosmetics })
+      ),
+    [cosmetics]
+  );
 
-    const getUrl = useCallback(() => {
-        const url = new URL(atob(G_A_M_E__U_R_L__B_A_S_E_6_4));
+  const getUrl = useCallback(() => {
+    const url = new URL(atob(G_A_M_E__U_R_L__B_A_S_E_6_4));
 
-        url.searchParams.set("level", cosmetics.toString());
+    url.searchParams.set("level", cosmetics.toString());
 
-        if (connectedAccountAddress) {
-            url.searchParams.set("address", connectedAccountAddress.toString());
-        }
+    if (connectedAccountAddress) {
+      url.searchParams.set("address", connectedAccountAddress.toString());
+    }
 
-        return url;
-    }, [cosmetics, connectedAccountAddress]);
+    return url;
+  }, [cosmetics, connectedAccountAddress]);
 
-    const contextValue = useMemo<MinigameContextValue>(
-        () => ({
-            cosmetics,
-            getUrl,
-            setCosmetics,
-        }),
-        [cosmetics, setCosmetics, getUrl]
-    );
+  const contextValue = useMemo<MinigameContextValue>(
+    () => ({
+      cosmetics,
+      getUrl,
+      setCosmetics,
+    }),
+    [cosmetics, setCosmetics, getUrl]
+  );
 
-    return (
-        <MinigameContext.Provider value={contextValue}>
-            {children}
-        </MinigameContext.Provider>
-    );
+  return (
+    <MinigameContext.Provider value={contextValue}>
+      {children}
+    </MinigameContext.Provider>
+  );
 };
 
 export const useMinigame = () => useContext(MinigameContext);
