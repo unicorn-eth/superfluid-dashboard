0b6eb9a628d8ec9f3e0d2cfea1dda6edfc448215
Philip Andersson <philip@superfluid.finance>
2023-11-16 11:36:13 +0100

    fix: add USDC.ex to token list even if not listed for Polygon (#692)

--- src/features/tokenWrapping/useTokenPairsQuery.tsx
+++ src/features/tokenWrapping/useTokenPairsQuery.tsx
@@ -3,15 +3,27 @@ import { useNetworkCustomTokens } from "../customTokens/customTokens.slice";
 import { getNetworkDefaultTokenPair, Network, networkDefinition } from "../network/networks";
 import { subgraphApi } from "../redux/store";
 
-const Arbitrum_USDC_ex = "0x1dbc1809486460dcd189b8a15990bca3272ee04e";
-
 export const useTokenPairsQuery = ({ network }: { network: Network }) => {
   const networkCustomTokens = useNetworkCustomTokens(network.id);
   const defaultTokenPair = getNetworkDefaultTokenPair(network);
 
   const unlistedTokenIDs = useMemo(
-    // A temporary fix for re-deployment of USDCx on Arbitrum. Issue: https://github.com/superfluid-finance/superfluid-dashboard/issues/687
-    () => network.id === networkDefinition.arbitrum.id ? networkCustomTokens.concat(Arbitrum_USDC_ex) : networkCustomTokens,
+    // A temporary fix for re-deployment of USDCx on Arbitrum & Polygon.
+    // Issue applies to both chains: https://github.com/superfluid-finance/superfluid-dashboard/issues/687
+    () => {
+      const Arbitrum_USDC_ex = "0x1dbc1809486460dcd189b8a15990bca3272ee04e";
+      const Polygon_USDC_ex = "0xCAa7349CEA390F89641fe306D93591f87595dc1F";
+
+      if (network.id === networkDefinition.arbitrum.id) {
+        return networkCustomTokens.concat(Arbitrum_USDC_ex);
+      }
+      
+      if (network.id === networkDefinition.polygon.id) {
+        return networkCustomTokens.concat(Polygon_USDC_ex);
+      }
+
+      return networkCustomTokens;
+    },
     [networkCustomTokens, network.id]
   );
 
